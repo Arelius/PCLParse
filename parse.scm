2c331e6c16b46a1edd70ed5f4282641a8488074e
Nicholas Indy Ray <indy@ishere.com>
2009-02-07 19:26:14 -0800

    Fixed many parser, and combinator bugs.

--- parse.scm
+++ parse.scm
@@ -1,6 +1,8 @@
 ;; utility macros
 
-(require-extension riaxpander)
+(require srfi/9)
+
+;;(require-extension riaxpander)
 
 (define-syntax while
   (syntax-rules ()
@@ -8,23 +10,23 @@
      ((while cond body ...) (letrec ((loop (lambda () (if cond (begin body ... (loop)) #f)))) (loop)))))
 
 (define-syntax rec-lambda
-  (er-macro-transformer
-   (lambda (e r c)
-     `(,(r 'letrec) ((rec-call (,(r 'lambda) ,(cadr e) ,@(cddr e))))
-       rec-call))))
+  (syntax-rules ()
+          ((rec-lambda (name args ...) body ...) (letrec ((name (lambda (args ...) body ...)))
+                                                  name))))
 
 ;; stream handling
 
 (define-record-type parse-stream
   (make-parse-stream file in-port read-stack)
   parse-stream?
-  (file get-file)
+  (file parse-stream-get-file)
   (in-port get-in-port set-in-port!)
   (read-stack get-read-stack set-read-stack!))
 
 (define (open-parse-stream file) (make-parse-stream file (open-input-file file) '(0)))
 
-(define (get-next-char in) (set-car! (get-read-stack in) (+ 1 (car (get-read-stack in))))
+(define (get-next-char in) 
+  (set-read-stack! in (cons (+ 1 (car (get-read-stack in))) (cdr (get-read-stack in))))
   (read-char (get-in-port in)))
 
 (define (push-stream-frame! in)
@@ -33,13 +35,13 @@
 (define (discard-stream-frame! in)
   (set-read-stack! in (cdr (get-read-stack in)))
   (close-input-port (get-in-port in))
-  (set-in-port! in (open-input-file))
-  (rec-lambda (i)
+  (set-in-port! in (open-input-file (parse-stream-get-file in)))
+  ((rec-lambda (rec-call i)
               (if (eq? (car (get-read-stack in)) i)
                   #f
                   (begin
                     (read-char (get-in-port in))
-                    (rec-call (+ i 1))))))
+                    (rec-call (+ i 1))))) 0))
 
 
 (define (merge-stream-frame! in)
@@ -70,30 +72,31 @@
 
 (define (parse-any-char chars in)
   (if (null? chars)
-      #t
+      #f
       (let ((out (parse-a-char (car chars) in)))
         (if out
-            (parse-any-char (cdr chars) in)
-            #f))))
+            out
+            (parse-any-char (cdr chars) in)))))
 
 (define (parse-whitespace in)
-  (parse-any-char '(#\space #\tab #\newline)))
+  (parse-any-char '(#\space #\tab #\newline) in))
 
 (define (parse-str str in)
   (if (eq? (string-length str) 0)
       #t
-      (if (parse-a-char (string-ref str 0))
+      (if (parse-a-char (string-ref str 0) in)
           (if (parse-str (substring str 1 (string-length str)) in)
               str
-              #f))))
+              #f)
+          #f)))
 
 (define (parse-num-char in)
-  (parse-any-char '(0 1 2 3 4 5 6 7 8 9) in))
+  (parse-any-char (string->list "0123456789") in))
 
 ;; parser combinators
 
 (define (parse-context parser)
-  (lambda (in) (call-with-parse-stack-frame parser in)))
+  (lambda (in) (call-with-parse-stack parser in)))
 
 (define (parse-or . parser)
   (lambda (in)
@@ -106,10 +109,14 @@
 
 (define (parse-* parser)
   (letrec ((parser-loop (lambda (in)
-                          (let ((out (parse-context (parser in))))
+                          (let ((out (call-with-parse-stack parser in)))
                             (if out
                                 (cons out (parser-loop in))
-                                '())))))))
+                                '())))))
+           (lambda (in) (let ((out (parser-loop in)))
+                          (if (eq? out '())
+                              #f
+                              out)))))
 
 (define (parse-last . parser)
   (lambda (in)
@@ -122,19 +129,30 @@
 
 (define (parse-sequence . parser)
   (lambda (in)
-    (let ((out ((car parser) in))
+    (let* ((out ((car parser) in))
           (ret (if (and out (cdr parser))
                ((parse-sequence (cdr parser)) in)
-               #f))))
-    (if (cdr parser)
-        (if ret
-            (cons out ret)
-            #f)
-        (cons out '()))))
+               #f)))
+      (if (and (cdr parser) (not ret))
+          #f
+          (cons out ret)))))
 
 ;; Additional parsers
 
-(define parse-num )
+(define (parse-num in)
+  (let ((ret ((parse-sequence
+                                  (parse-* parse-num-char)
+                                  (lambda (in) (parse-a-char #\.))
+                                  (parse-* parse-num-char)) in)))
+    (if ret 
+        (string->number (list->string ret))
+        #f)))
+
+(define (parse-int in)
+  (let ((ret ((parse-* parse-num-char) in)))
+    (if ret
+        (string->number (list->string ret))
+        #f)))
 
 (define parse-eat-whitespace (parse-* parse-whitespace))
 
@@ -148,7 +166,7 @@
   (y get-line-y set-line-y!)
   (len get-line-len set-line-len!))
 
-(define parse-space-num in (parse-last parse-eat-whitespace parse-num))
+(define parse-space-num (parse-last parse-eat-whitespace parse-num))
 
 (define (parse-a-line str sym in) 
   (parse-eat-whitespace in)
@@ -166,3 +184,11 @@
                             parse-text))
 
 (define (parse-pcl) (parse-* parse-command))
+
+(define test-in (open-parse-stream "test.pcl"))
+(call-with-parse-stack (lambda (in) (parse-str "tesj" in)) test-in)
+(call-with-parse-stack (lambda (in) (parse-str "test" in)) test-in)
+(call-with-parse-stack (lambda (in) (parse-str "test" in)) test-in)
+(parse-eat-whitespace test-in)
+(call-with-parse-stack (lambda (in) (parse-whitespace in)) test-in)
+(call-with-parse-stack parse-num test-in)
